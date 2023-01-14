07e23dd84968eafdcb47c26def6580b77f8c202a
Eliot <ef@eliotfondere.com>
2023-01-14 18:50:30 -0500

    Menus (Level & Main)

---
+++ Assets/Scripts/GameStateManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameStateManager : MonoBehaviour
+{
+    public static bool isInDream()
+    {
+        return false;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
