c283d0285cb8b75d064372beb9e5133e9af94c9b
safasifa <sofianm.visualstudio@gmail.com>
2023-01-14 19:40:24 -0500

    Incomplete Block Controller

--- Assets/Scripts/playerController.cs
+++ Assets/Scripts/playerController.cs
@@ -13,6 +13,11 @@ public class playerController : MonoBehaviour
 
     Vector2 moveDir;
     float yVelocity = 0;
+    private GameObject currentBlock;
+    public playerBlocksManager playerBlocksManager;
+    int currentBlockIndex = 0;
+    public float blockMoveStep;
+
 
     private void Start()
     {
@@ -33,26 +38,55 @@ public class playerController : MonoBehaviour
             yVelocity -= Time.fixedDeltaTime * gravityScale;
         }
         else if (!isJumping)
-        { 
+        {
             yVelocity = 0f;
         }
-        else if (isJumping) {
-            isJumping = false;
-        
-        }
-        if (Input.GetButton("Jump"))
+        else if (isJumping)
         {
-            if (isGrounded())
-            {
-                isJumping = true;
-                yVelocity = jumpVelocity;
-            }
+            isJumping = false;
 
         }
-        rb.MovePosition(rb.position + moveDir*moveSpeed * Time.fixedDeltaTime + Vector2.down*gravityScale* 0.5f *Mathf.Pow(Time.fixedDeltaTime, 2) + Vector2.up * yVelocity*Time.fixedDeltaTime);
-      
+
+        if (!Input.GetButton("Fire3"))
+        {
+            if (Input.GetButton("Jump"))
+            {
+                if (isGrounded())
+                {
+                    isJumping = true;
+                    yVelocity = jumpVelocity;
+                }
+
+            }
+            rb.MovePosition(rb.position + moveDir * moveSpeed * Time.fixedDeltaTime + Vector2.down * gravityScale * 0.5f * Mathf.Pow(Time.fixedDeltaTime, 2) + Vector2.up * yVelocity * Time.fixedDeltaTime);
+        }
+        else
+        {
+            EditBlock();
+        }
     }
 
+
+    private void EditBlock()
+    {
+        Debug.Log("Edit Block");
+        if (Input.GetButtonDown("Z"))
+        {
+            Debug.Log("ZZ");
+            if (currentBlockIndex == playerBlocksManager.blocks.Length - 1)
+            {
+                currentBlockIndex = 0;
+            }
+            else
+            {
+                currentBlockIndex++;
+                currentBlock.SetActive(false);
+            }
+        }
+        currentBlock = playerBlocksManager.blockList[currentBlockIndex][0];
+        currentBlock.SetActive(true);
+        currentBlock.transform.position += new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")) * blockMoveStep;
+    }
     private bool isGrounded()
     {
         if (groundChecker.IsTouchingLayers(LayerMask.GetMask("Ground")))
