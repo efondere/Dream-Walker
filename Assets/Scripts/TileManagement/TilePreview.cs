4b524f0951560a7a389a504d9d4a02eb7a91b52b
Eliot <ef@eliotfondere.com>
2023-06-20 21:47:24 -0400

    Add Tile Rules

--- Assets/Scripts/TileManagement/TilePreview.cs
+++ Assets/Scripts/TileManagement/TilePreview.cs
@@ -22,6 +22,6 @@ public class TileGrid
 
 public class TilePreview : MonoBehaviour
 {
-    [SerializeField] public Tile[] tiles;
+    [SerializeField] public TileBase[] tiles;
     [SerializeField] public TileGrid grid;
 }
